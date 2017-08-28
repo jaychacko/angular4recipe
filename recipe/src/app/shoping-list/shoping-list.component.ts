a19092c6534b9a50ccb81e5ee960ddba74b7a35b
jaisil007 <jaisilchacko@yahoo.com>
2017-08-28 15:43:06 -0500

    more code

--- recipe/src/app/shoping-list/shoping-list.component.ts
+++ recipe/src/app/shoping-list/shoping-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Ingredient} from '../shared/shoping-list-modal';
 @Component({
   selector: 'app-shoping-list',
   templateUrl: './shoping-list.component.html',
   styleUrls: ['./shoping-list.component.css']
 })
 export class ShopingListComponent implements OnInit {
-
+  ingredients : Ingredient[] = [
+    new Ingredient('Apples',5),
+    new Ingredient('Tommato',4),
+    
+  ]
   constructor() { }
 
   ngOnInit() {
